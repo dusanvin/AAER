7d05f5deee5c4712d1d55bc57f27b6f9bd55fed1
lumlop <vincent.dusanek@gmail.com>
2020-09-18 12:48:09 +0200

    Integration Tooltips
    Integration Tooltips in Navigation und index.html

--- js/navigation.js
+++ js/navigation.js
@@ -17,25 +17,29 @@ function display_navigation() {
 
 				<li class="nav-item pl-5 pr-5 mt-3 mb-3">
 
-					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="index.html">Home</a>
+					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="index.html" data-toggle="tooltip" data-placement="bottom"
+  title="Zur Startseite">Home</a>
 
 				</li>
 
 				<li class="nav-item pl-5 pr-5 mt-3 mb-3">
 
-					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link text-center" href="survey.html">Zum AAER</a>
+					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link text-center" href="survey.html" data-toggle="tooltip" data-placement="bottom"
+  title="Analysieren und bewerten Sie ein Bildungsmedium">Zum AAER</a>
 
 				</li>
 
 				<li class="nav-item pl-5 pr-5 mt-3 mb-3">
 
-					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="info.html">Mehr Erfahren</a>
+					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="info.html" data-toggle="tooltip" data-placement="bottom"
+  title="Erfahren Sie mehr über das AAER">Mehr Erfahren</a>
 
 				</li>
 				
 				<li class="nav-item pl-5 pr-5 mt-3 mb-3">
 
-					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="contact.html">Kontakt</a>
+					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="contact.html" data-toggle="tooltip" data-placement="bottom"
+  title="Kontaktieren Sie uns bei Fragen rund um das AAER">Kontakt</a>
 
 				</li>
 
@@ -68,3 +72,8 @@ $(document).ready(function() {
 // Ende: Hervorhebung des Active-Elements in der Navigation
 
 display_navigation();
+
+// Tooltips Initialization
+$(function () {
+$('[data-toggle="tooltip"]').tooltip()
+})
