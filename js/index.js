dbc62790c6436a84e368cc0007356a930699ecf8
sn2016 <sn2016@users.noreply.github.com>
2020-09-21 02:37:28 +0200

    pdf geht mit chrome, aber bei adobe reader kommt fehler

--- js/index.js
+++ js/index.js
@@ -708,8 +708,21 @@ function downloadPdf() {
     // contextPdf.drawImage(chart6canvas, 39, 39+170+340+340, 340, 340);
     // contextPdf.drawImage(chart7canvas, 389, 39+170+340+340, 340, 340);
 
+    // only jpeg is supported by jsPDF
+    // var imgData = canvasPdf.toDataURL("image/jpeg", 1.0);
+    // var pdf = new jsPDF();
+    //
+    // pdf.addImage(imgData, 'JPEG', 0, 0);
+    // pdf.save("download.pdf");
 
-    // var doc = new jsPDF('p', 'mm'; 'a4';
+    var imgData = canvasPdf.toDataURL("image/png", 1.0);
+    var pdf = new jsPDF();
+
+    pdf.addImage(imgData, 'PNG', 0, 0);
+    pdf.save("download.pdf");
+
+
+    // var doc = new jsPDF('p', 'mm');
     // html2canvas($("#canvasPdf"), {
     //     onrendered: function(canvas) {
     //         var imgData = canvas.toDataURL(
