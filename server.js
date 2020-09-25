4e295df230ed18c73041cfc5a4f6228d6e1f1c49
sn2016 <sn2016@users.noreply.github.com>
2020-09-25 19:55:08 +0200

    aufgeräumt

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 var Express = require('express');
 var Fs = require('fs');
 
+// const mariadb = require('mariadb');
+//
+// const pool = mariadb.createPool({
+//     host: 'mydb.com',
+//     user:'myUser',
+//     password: 'myPassword',
+//     connectionLimit: 5
+// });
+//
+// async function asyncFunction() {
+//     let conn;
+//     try {
+//         conn = await pool.getConnection();
+//         const rows = await conn.query("SELECT 1 as val");
+//         console.log(rows); //[ {val: 1}, meta: ... ]
+//         const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+//         console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+//
+//     } catch (err) {
+//         throw err;
+//     } finally {
+//         if (conn) return conn.end();
+//     }
+// }
+
 
 
 const port = 3000;
