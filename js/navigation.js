a6a46de3cb8c4560fe6c27f0f4836ec0370afa50
lumlop <vincent.dusanek@gmail.com>
2023-08-30 10:39:05 +0200

    Finished New Responsive Menu Bar in DE / EN with logo
    Finished New Responsive Menu Bar in DE / EN with logo

--- js/navigation.js
+++ js/navigation.js
@@ -2,175 +2,91 @@ function display_navigation() {
 	var container = document.getElementById("navigation_container");
 	container.innerHTML =
 		`
-<!-- Navigation Start -->
-	<link rel = "stylesheet" href = "/css/navigation.css" />
-	<div class="site-navbar-top">
-		<div class="container py-3">
-			<div class="row align-items-center">
-				<div class="col-6">
+		<!-- Navigation Start -->
+		<link rel = "stylesheet" href = "/css/navigation.css" />
+		<div class="site-navbar-top">
+			<div class="container py-3 px-2">
+				<div class="row align-items-center mx-0">
 					<div class="d-flex mr-auto">
 						<a href="mailto:team@digillab.uni-augsburg.de" class="d-flex align-items-center mr-4">
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2" viewBox="0 0 16 16">
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2 d-none d-md-inline-block" viewBox="0 0 16 16">
 							  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
 							</svg>
 							<span class="d-none d-md-inline-block">team@digillab.uni-augsburg.de</span>
 						</a>
 						<a href="tel:+498215983151" class="d-flex align-items-center mr-auto">
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2" viewBox="0 0 16 16">
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2 d-none d-md-inline-block" viewBox="0 0 16 16">
 							  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
 							</svg>
 							<span class="d-none d-md-inline-block"> +49 821 598 3151</span>
 						</a>
 					</div>
-				</div>
-				<div class="col-6 text-right">
-					<div class="mr-auto d-inline-flex align-items-center text-white font-weight-bold">
-						<a href="javascript:clickedGerman()">DE</a>
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mx-2" viewBox="0 0 16 16">
-						  <path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z"/>
-						</svg>
-						<a href="javascript:clickedEnglish()">EN</a>
+					<div class="d-flex ml-auto">
+						<a href="#" class="d-flex align-items-center">
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2" viewBox="0 0 16 16">
+							  <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286H4.545zm1.634-.736L5.5 3.956h-.049l-.679 2.022H6.18z"/>
+							  <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2zm7.138 9.995c.193.301.402.583.63.846-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6.066 6.066 0 0 1-.415-.492 1.988 1.988 0 0 1-.94.31z"/>
+							</svg>
+							<div id="dropdown-languages_de" class="mr-auto d-inline-flex align-items-center text-white font-weight-bold">
+								<a href="javascript:clickedGerman()">DE</a>
+							</div>
+							<span class="text-white mx-1">/</span>
+							<div id="dropdown-languages_en" class="mr-auto d-inline-flex align-items-center text-white font-weight-bold">
+								<a href="javascript:clickedEnglish()">EN</a>
+							</div>
+						</a>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-
-<div class="container">
-	<nav class="navbar navbar-expand-lg pt-3 mt-0 navbar-dark mb-3">
-	  	<a data-lang="De" class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="index.html" data-toggle="tooltip" data-placement="bottom" title="Zur Startseite" alt="Start">
-			Startseite
-		</a>
-		<a style="display: none;" data-lang="En" class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="index.html" data-toggle="tooltip" data-placement="bottom" title="Zur Startseite" alt="Start">
-			Home
-		</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
-
-	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-	    <ul class="navbar-nav mr-auto">
-	      <li class="nav-item active">
-	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="#">Link</a>
-	      </li>
-	      <li class="nav-item dropdown">
-	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	          Dropdown
-	        </a>
-	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-	          <a class="dropdown-item" href="#">Action</a>
-	          <a class="dropdown-item" href="#">Another action</a>
-	          <div class="dropdown-divider"></div>
-	          <a class="dropdown-item" href="#">Something else here</a>
-	        </div>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-	      </li>
-	    </ul>
-	    <form class="form-inline my-2 my-lg-0">
-	      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-	    </form>
-	  </div>
-	</nav>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<div class="container">
-<nav class="navbar navbar-expand-lg pt-3 mt-0 navbar-dark mb-3">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse justify-content-md-center row" id="navbarNavAltMarkup">
-			<div class="col-2">
-				<a data-lang="De" class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="index.html" data-toggle="tooltip" data-placement="bottom" title="Zur Startseite" alt="Start">
-					Startseite
-				</a>
-				<a style="display: none;" data-lang="En" class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="index.html" data-toggle="tooltip" data-placement="bottom" title="Zur Startseite" alt="Start">
-					Home
-				</a>
-			</div>
-			<ul class="navbar-nav col-10">
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="De">
-					<a class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="index.html" data-toggle="tooltip" data-placement="bottom" title="Zur Startseite" alt="Start">
-						Startseite
-					</a>
-				</li>
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="En" style="display: none;">
-					<a class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="index.html" data-toggle="tooltip" data-placement="bottom" title="Go to startpage" alt="Start">
-						Home
-					</a>
-				</li>
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="De">
-					<a class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="survey.html" data-toggle="tooltip" data-placement="bottom" title="Analysieren und bewerten Sie ein Bildungsmedium" alt="Zum AAER">
-						Zum AAER
-					</a>
-				</li>
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="En" style="display: none;">
-				<a class="aaer_menu pt-2 d-md-inline-block text-white navigation_link text-center" href="survey.html" data-toggle="tooltip" data-placement="bottom" title="Analyze and evaluate an OER" alt="Go to the AAER">
-					Go to the AAER
-				</a>
-			</li>
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="De">
-					<a class="aaer_menu pt-2 d-md-inline-block text-white navigation_link" href="info.html" data-toggle="tooltip" data-placement="bottom" title="Erfahren Sie mehr über das AAER" alt="Mehr erfahren">
-						Mehr
-  					</a>
-				</li>
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="En" style="display: none;">
-				<a class="aaer_menu pt-2 d-md-inline-block text-white navigation_link" href="info.html" data-toggle="tooltip" data-placement="bottom" title="more" alt="more">
-					More
-				</a>
-			</li>
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="De">
-					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="contact.html" data-toggle="tooltip" data-placement="bottom" title="Kontaktieren Sie uns bei Fragen rund um das AAER" alt="Kontakt">
-						Kontakt
-					</a>
-				</li>
-				<li class="nav-item pl-5 pr-5 mt-3 mb-3" data-lang="En" style="display: none;">
-					<a class="aaer_menu py-2 d-md-inline-block text-white navigation_link" href="contact.html" data-toggle="tooltip" data-placement="bottom" title="Contact us if there are any questions about the AAER"  alt="Contact">
-						Contact
-					</a>
-			</li>
-				
-
-  			</ul>
-		  </div>
-		</nav>
-
+		<div class="container px-2">
+			<nav class="navbar navbar-expand-lg pt-3 mt-0 navbar-dark mb-3 pr-0">
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    <ul class="navbar-nav mr-auto">
+			      <li class="nav-item">
+			        <a data-lang="De" class="nav-link pl-0 pb-0" href="index.html" data-toggle="tooltip" data-placement="bottom" title="Zur Startseite" alt="Start">
+			        	<div class="image">
+			          		<figure class="mb-0">
+			            		<img class="image-img" src="/img/sophie/AAER%20Logo.png" width="200">
+			          		</figure>
+			        	</div>
+			        </a>
+			        <a style="display: none;" data-lang="En" class="nav-link pl-0 pb-0" href="index.html" data-toggle="tooltip" data-placement="bottom" title="GoTo: Startpage" alt="Start">
+			        	<div class="image">
+			          		<figure class="mb-0">
+			            		<img class="image-img" src="/img/sophie/AAER%20Logo.png" width="200">
+			          		</figure>
+			        	</div>
+			        </a>
+			      </li>
+			    </ul>
+			    <ul class="navbar-nav mt-3">
+			    	<li class="nav-item" data-lang="De">
+			        	<a class="nav-link" href="survey.html" data-toggle="tooltip" data-placement="bottom" title="Analysieren und bewerten Sie ein Bildungsmedium" alt="Analysieren und bewerten Sie ein Bildungsmedium">Zum AAER</a>
+			      	</li>
+			    	<li class="nav-item" data-lang="En" style="display: none;">
+			        	<a class="nav-link" href="survey.html" data-toggle="tooltip" data-placement="bottom" title="Analyze and evaluate an Open Educational Resource" alt="Go to the AAER">Go to: AAER</a>
+			      	</li>
+					<li class="nav-item" data-lang="De">
+			        	<a class="nav-link" href="info.html" data-toggle="tooltip" data-placement="bottom" title="Erfahren Sie mehr über das AAER" alt="Erfahren Sie mehr über das AAER">Mehr</a>
+			      	</li>
+			    	<li class="nav-item" data-lang="En" style="display: none;">
+			        	<a class="nav-link" href="info.html" data-toggle="tooltip" data-placement="bottom" title="Learn more about the AAER" alt="Learn more about the AAER">More</a>
+			      	</li>
+					<li class="nav-item" data-lang="De">
+			        	<a class="nav-link" href="contact.html" data-toggle="tooltip" data-placement="bottom" title="Kontaktieren Sie uns bei Fragen" alt="Kontaktieren Sie uns bei Fragen">Kontakt</a>
+			      	</li>
+			    	<li class="nav-item" data-lang="En" style="display: none;">
+			        	<a class="nav-link" href="contact.html" data-toggle="tooltip" data-placement="bottom" title="Contact us if you have any questions" alt="Contact us if you have any questions">Contact</a>
+			      	</li>
+			     </ul>
+			  </div>
+			</nav>
 		</div>
 		<!-- Navigation End -->
-
-
-
-
-
-
-
-
-
-
-
-
-    
 		`
 	;
 }
